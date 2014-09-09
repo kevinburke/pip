c6fddb019359fbd22f95350dbc4a0f3b63866c15
Kevin Burke <kev@inburke.com>
2014-09-09 16:18:36 -0700

    try running in debug mode

--- tests/functional/test_install_user.py
+++ tests/functional/test_install_user.py
@@ -234,7 +234,8 @@ class Tests_UserSite:
         script.pip('install', 'INITools==0.2')
         script.pip('install', '--user', 'INITools==0.3')
 
-        result3 = script.pip('install', '--user', 'INITools==0.1')
+        script.verbose = True
+        result3 = script.pip('install', '--user', 'INITools==0.1', debug=True)
 
         # usersite has 0.1
         egg_info_folder = (
